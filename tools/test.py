8de493da2649fe7c3b91cf6a40430f6f43462b97
Stillwtm <2454340659@qq.com>
2023-01-30 15:13:32 +0800

    fix bugs

--- tools/test.py
+++ tools/test.py
@@ -1,8 +1,12 @@
 from got10k.experiments import *
 from siamfc import TrackerSiamFC
 
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+
 if __name__ == '__main__':
-    net_path = ""
+    net_path = "/home/snorlax/Projects/SiamFC-pytorch/siamfc_alexnet_e50.pth"
+    # net_path = "/home/snorlax/Projects/SiamFC-pytorch/models/SiamFC_100.pth"
     data_dir = "/home/snorlax/datasets/track/OTB100"
 
     track = TrackerSiamFC(net_path)
